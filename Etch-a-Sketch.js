ce88f0fb28d137470ccae67255fa00225efb4628
Daisy-Opanga <daisyopanga2@gmail.com>
2024-10-21 13:19:51 +0300

    modified:   Etch-a-Sketch.js added a hover effect on the grid

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -6,7 +6,17 @@ function createGrid(){
         const gridSqr = document.createElement("div");
         gridSqr.classList.add("grid-square");
         container.appendChild(gridSqr);
+        
     }
 }
 
+function createHoverEffect() {
+    const gridSqrs = document.querySelectorAll(".grid-square");
+    gridSqrs.forEach((gridSqr) => {
+    gridSqr.addEventListener("mouseover",() =>{
+        gridSqr.style.backgroundColor = "rgb(197, 124, 124)";
+    });
+}); 
+}
 createGrid();
+createHoverEffect();
